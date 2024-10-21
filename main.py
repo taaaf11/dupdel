ad0a66bd292813fefd77f7a44ab76b27f2c73a9d
taaaf11 <taafuuu@gmail.com>
2024-10-21 06:23:23 +0500

    Add type hints

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from __future__ import annotations
+
 import filecmp
 import os
 import re
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from re import Match
+
 
 # similar: The files are apparently duplicate (by their names)
 # identical: The files are similar and their contents are also same
@@ -9,26 +16,26 @@ original_regex = re.compile(r'\s*\([1-9]+\)\s*')
 duplicate_regex = re.compile(r'\w+\s*\([1-9]+\)\s*.*')
 
 
-def detect_similar(path: str):
+def detect_similar(path: str) -> Match:
     return duplicate_regex.fullmatch(os.path.basename(path))
 
 
-def similarly_original_exists(path: str):
+def similarly_original_exists(path: str) -> bool:
     return os.path.exists(make_similar_original_path(path))
 
 
-def identical_exists(duplicate_path: str):
+def identical_exists(duplicate_path: str) -> bool:
     similar_original_path = make_similar_original_path(duplicate_path)
     return similarly_original_exists(duplicate_path) and filecmp.cmp(similar_original_path, duplicate_path, False)
 
 
-def make_similar_original_path(path: str):
+def make_similar_original_path(path: str) -> str:
     parent, basename = os.path.split(path)
     original_basename = original_regex.sub('', basename)
     return os.path.join(parent, original_basename)
 
 
-def main():
+def main() -> None:
     filenames = os.listdir()
     duplicates = [filename for filename in filenames if detect_similar(filename)]
 
